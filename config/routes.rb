e43f3659682b78e7dbac8ccfccff975635cab0d9
Ranika Williams <rnkprice07@gmail.com>
2019-11-13 15:58:03 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,28 @@
 Rails.application.routes.draw do
+  get 'patients/index'
+
+  get 'patients/show'
+
+  get 'patients/new'
+
+  get 'patients/edit'
+
+  get 'appointments/index'
+
+  get 'appointments/show'
+
+  get 'appointments/new'
+
+  get 'appointments/edit'
+
+  get 'doctors/index'
+
+  get 'doctors/show'
+
+  get 'doctors/new'
+
+  get 'doctors/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doctors
   resources :patients
